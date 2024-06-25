5925372416da425f8401c07ceefab17a6bf09227
Agung Satriawan <45866024+agungSatriawan@users.noreply.github.com>
2024-06-25 16:11:46 +0700

    add assets

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+$('body').on('click', '.login', function (e) {
+    e.preventDefault();
+    let username = $('.username').val();
+    let password = $('.password').val();
+    if (username != '' && password != '') {
+
+    }
+})
